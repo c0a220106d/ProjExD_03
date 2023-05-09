f1b7ff0d9249898d4e564a111a25c5f7c6aca789
c0a22010 <c0a220106d@edu.teu.ac.jp>
2023-05-09 14:50:36 +0900

    練習2

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -168,6 +168,7 @@ def main():
             if beam._rct.colliderect(bomb._rct):
                 beam = None
                 bomb = None 
+                bird.change_img(6, screen)
         pg.display.update()
         clock.tick(1000)
 
